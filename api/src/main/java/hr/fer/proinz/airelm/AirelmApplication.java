bd1607a206b37dd6001a4eb45db9084d34a2456e
Marko Flegar <185770897+mflegar@users.noreply.github.com>
2024-11-13 03:08:39 +0100

    chore: init backend
    https://start.spring.io/

---
+++ api/src/main/java/hr/fer/proinz/airelm/AirelmApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.proinz.airelm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirelmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirelmApplication.class, args);
+	}
+
+}
