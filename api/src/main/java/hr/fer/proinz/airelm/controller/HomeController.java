030d1c77c370f8312505aaeffcfefa9d20557b94
antonio.tolic <antonio.tolic@fer.unizg.hr>
2024-11-14 07:53:21 +0100

    Added OAuth2 login

---
+++ api/src/main/java/hr/fer/proinz/airelm/controller/HomeController.java
@@ -0,0 +1,26 @@
+package hr.fer.proinz.airelm.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/") // Pocetna ruta
+    public String greet(){
+        return "Welcome to the page";
+    }
+
+    @GetMapping("/login2") // Ruta /login2
+    public String login(@AuthenticationPrincipal OAuth2User principal){
+        // Dobivamo podatke korisnika koje je GitHub poslao
+        Map<String, Object> attributes = principal.getAttributes();
+
+        // Ispisivanje korisničkih podataka kao string
+        return "GitHub User Info: " + attributes.toString();
+    }
+}
