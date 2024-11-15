09cd53df8cf909703257c998ab1808367bf2ce65
antonio.tolic <antonio.tolic@fer.unizg.hr>
2024-11-15 13:09:53 +0100

    Added privacy to Github app, tried fixing CORS POLICY
    Added privacy to OAuth2 app settings, Tried to fix CORS policy error but no success, changed App.tsx a bit

--- api/src/main/java/hr/fer/proinz/airelm/controller/HomeController.java
+++ api/src/main/java/hr/fer/proinz/airelm/controller/HomeController.java
@@ -2,12 +2,14 @@ package hr.fer.proinz.airelm.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5780") // Ovo ne radi nazalost
 public class HomeController {
 
     @GetMapping("/") // Pocetna ruta
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UserForm from './UserForm';
-import Researcher from './Researcher';
-import InstitutionManager from './InstitutionManager';
 import './App.css';  // Importiraj CSS za App
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<h2>Home Page</h2>} />  {/* Početna stranica */}
+                <Route path="/" element={<h2>Home Page</h2>} />  {/* Pocetna stranica */}
                 <Route path="/form" element={<UserForm />} />  {/* Forma na /form */}
-                <Route path="/researcher" element={<Researcher />} />  {/* Stranica za Researcher */}
-                <Route path="/institution-manager" element={<InstitutionManager />} />  {/* Stranica za Institution Manager */}
+                <Route path="/researcher" element={<h2>Researcher!</h2>} />  {/* Stranica za Researcher */}
+                <Route path="/institution-manager" element={<h2>Institution!</h2>}/> {/* Stranica za Institution Manager */}
             </Routes>
         </Router>
     );
