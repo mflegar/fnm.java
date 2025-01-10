b25376369ef27d5a0ffadf7d344e88f8f14e3bec
VedranMaksic <vedran.maksic@fer.unizg.hr>
2025-01-10 20:22:18 +0100

    adding ActorRoleInstitution
    addig repo, service, adding actorroleinstitution to database with institution controller

--- api/src/main/java/hr/fer/proinz/airelm/repository/ProjectRepository.java
+++ api/src/main/java/hr/fer/proinz/airelm/repository/ProjectRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
@@ -14,4 +15,5 @@ public interface ProjectRepository extends JpaRepository<Project, Integer> {
 
     List<Project> findByInstitution_InstitutionID(Integer institutionID);
 
+    Optional<Project> findByProjectName(String name);
 }
